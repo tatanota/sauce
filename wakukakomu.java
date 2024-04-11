b87a43b4358da9d5de9891908f2d9c975475de7d
tatanota <158541120+tatanota@users.noreply.github.com>
2024-04-11 23:56:00 +0900

    Add files via upload

--- wakukakomu.java
+++ wakukakomu.java
@@ -1,22 +1,47 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        String un  = "";
-        
-        int con = line.length();
-        for (int n= 0 ; n<con+2  ;n++ ){
-            un += "+";
-        }
-        
-        
-        String kas = "+" + line + "+";
-        
-        System.out.println(un);
-        System.out.println(kas);
-        System.out.println(un);
-    }
+import java.util.Scanner;
+
+
+// 2024.4.11 paiza C084:【キャンペーン問題】枠で囲む
+
+public class wakukakomu {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+
+		while (true) {
+			System.out.println("文字を入力してEnterを押してください(終了する場合はEnterのみ): ");        
+
+
+			String line = sc.nextLine();
+
+
+			// Enterキーが押された場合、ループを抜けてプログラムを終了する
+			if (line.isEmpty()) {
+				break;
+			}
+
+
+			String un  = "";
+
+			//文字数取得
+			int con = line.length();
+
+			//文字数+2分"＋"を連結
+			for (int n= 0 ; n<con+2  ;n++ ){
+				un += "+";
+			}
+
+			//該当文字の両端に"＋"を連結
+			String kas = "+" + line + "+";
+
+			//表示
+			System.out.println(un);
+			System.out.println(kas);
+			System.out.println(un);
+		}
+
+		// プログラムが終了したことをユーザーに通知
+		System.out.println("終了します。");
+	}
 }
