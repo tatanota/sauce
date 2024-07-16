6c609f74c509d4a787cd19a2c3cde8e7234856d2
tatanota <158541120+tatanota@users.noreply.github.com>
2024-07-17 08:03:59 +0900

    Update Main.kt

--- Kotlin/Main.kt
+++ Kotlin/Main.kt
@@ -1,3 +1,22 @@
-fun main(args:Array<String>) {
-    println("Hello World！")
+// ゼロ・プラス・マイナスを繰り返し判定する
+fun main() {
+    println("繰り返したい回数を入力してください")
+
+    val count = readLine()!!.toInt()
+    println("入力された繰り返し回数: $count")
+
+    for (i in 1 .. count) {
+        println("確認したい数値を入力してください")
+
+        val number = readLine()!!.toInt()
+        println("入力された数値: $count")
+
+        if(number == 0){
+            println("${number}は0")
+        }else if(number > 0){
+            println("${number}はプラス")
+        }else{
+            println("${number}はマイナス")
+        }
+    }
 }
